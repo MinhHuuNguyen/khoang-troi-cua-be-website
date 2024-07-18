aeba662afb490ead8dedcfc794183db2a6787f50
TaiNguyen297 <tainguyen29702@gmail.com>
2024-07-19 00:05:59 +0700

    tim-hieu-cac-do-vat-trong-nha-cung-be-trai-nghiem

---
+++ src/utils/data/html/tim-hieu-cac-do-vat-trong-nha-cung-be-trai-nghiem.ts
@@ -0,0 +1,23 @@
+export default `
+<p>Cùng team Cùng Bé Trải Nghiệm hướng dẫn các bé khám phá và sáng tạo thông qua các đồ vật trong nhà thông qua hình dạng của nó. Chúng tôi khuyến khích các bé khám phá và hiểu hơn về các đồ vật trong cuộc sống hàng ngày thông qua việc phân loại chúng, tìm hiểu về vai trò của chúng trong xã hội. Đây là nơi mà các bé có thể thoải mái sáng tạo, phát triển tài năng hội họa của mình một cách thú vị nhất.</p>
+<h3>Các đồ vật trong nhà - khám phá hình dạng cơ bản và thế giới quanh ta</h3>
+<p>Trong phần này, chúng tôi sẽ dành thời gian để giúp các bé nhận biết và thực hành vẽ các hình dạng cơ bản thông qua các hoạt động sinh động và hấp dẫn. Dưới đây là những điểm chính mà chúng tôi sẽ khám phá:<br />
+Nhận biết hình dạng: giới thiệu với các bé những hình dạng cơ bản như hình vuông, hình tròn, hình tam giác, hình chữ nhật và hình ngôi sao thông qua hình ảnh và ví dụ trong cuộc sống hàng ngày.</p>
+<p>Thực hành vẽ lại: hướng dẫn cách vẽ lại chúng bằng cách sử dụng bút chì và giấy vẽ. Qua đó, chúng tôi khuyến khích sự sáng tạo và tự tin trong việc vẽ.</p>
+<p>Không chỉ dừng lại ở việc vẽ theo mẫu, chúng tôi cũng khuyến khích các bé sáng tạo và tự do biểu đạt ý tưởng của mình thông qua việc tạo ra những hình dạng mới và độc đáo.</p>
+<p><img class="size-full wp-image-467 aligncenter" src="http://ktcb.top/wp-content/uploads/2024/05/image_2024-03-26_160233367.png" alt="" width="612" height="408" /></p>
+<h3>Thông hiểu về những hoạt động thường ngày</h3>
+<p>Trong phần này, chúng tôi muốn giúp các bé không chỉ nhận biết hình dạng cơ bản của các đồ vật trong nhà mà còn hiểu về vai trò và ý nghĩa của chúng trong cuộc sống hàng ngày. Dưới đây là những hoạt động chính chúng tôi sẽ thực hiện:</p>
+<p>Phân loại đồ vật: Các bé sẽ được yêu cầu phân loại các đồ vật trong cuộc sống hàng ngày dựa trên hình dạng cơ bản mà chúng đã học.<br />
+Tìm hiểu về đồ vật: Chúng tôi sẽ thảo luận cùng các bé về các đồ vật và vị trí của chúng trong xã hội. Tại sao chúng ta cần những đồ vật này? Chúng có ý nghĩa gì trong cuộc sống hàng ngày của chúng ta?</p>
+<p><img class="size-full wp-image-469 aligncenter" src="http://ktcb.top/wp-content/uploads/2024/05/image_2024-03-26_155657945.png" alt="" width="600" height="397" /></p>
+<h3>Vận dụng và sáng tạo</h3>
+<p>Trong phần này, chúng tôi muốn khuyến khích các bé vận dụng những kiến thức đã học và phát triển khả năng sáng tạo của mình thông qua việc vẽ và tạo ra các tác phẩm hội họa. Dưới đây là những hoạt động chính mà chúng tôi sẽ tổ chức:</p>
+<p>Luyện tập vẽ và tô màu: Các bé sẽ được hướng dẫn vẽ lại hình dạng và các đồ vật trong nhà đã học trước đó. Chúng tôi cũng khuyến khích các bé sử dụng màu sắc để tô điểm và làm đẹp cho các tác phẩm của mình.</p>
+<p>Sáng tạo và vận dụng: Không chỉ giới hạn ở việc vẽ lại theo mẫu, chúng tôi cũng khuyến khích các bé sáng tạo và tự do biểu đạt ý tưởng của mình. Các bé được khuyến khích tự do chọn chủ đề và phát triển tác phẩm của mình theo cách riêng.</p>
+<p><img class="size-full wp-image-468 aligncenter" src="http://ktcb.top/wp-content/uploads/2024/05/image_2024-03-26_155750734.png" alt="" width="875" height="582" /></p>
+<h3>Củng cố và dặn dò</h3>
+<p>Sau những hoạt động sáng tạo và hấp dẫn, chúng tôi tiến hành phần củng cố và dặn dò để đảm bảo rằng mỗi học sinh có cơ hội thể hiện ý kiến và chia sẻ cảm nhận của mình về buổi học.</p>
+<p>Sau đó, các em được khuyến khích chia sẻ cảm nhận của mình về kiến thức mới, về sự hỗ trợ từ các anh chị giáo viên và về những trải nghiệm tích cực trong buổi học. Điều này giúp tạo ra một môi trường học tập tích cực và sẵn lòng chia sẻ.</p>
+<p>Chúng mình hy vọng rằng qua các hoạt động về một số sự vật đơn giản, các bé sẽ không chỉ nhận biết được các hình dạng cơ bản mà còn hiểu rõ hơn về thế giới xung quanh mình và vai trò của các đối tượng trong cuộc sống hàng ngày. Điều này sẽ giúp họ phát triển sự hiểu biết và tư duy logic từ nhỏ.</p>
+`
