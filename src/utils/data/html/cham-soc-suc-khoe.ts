4aaca537cbc7f125bf617a8666e7b7c8b587f3ed
TaiNguyen297 <tainguyen29702@gmail.com>
2024-07-19 15:59:38 +0700

    cham-soc-suc-khoe

---
+++ src/utils/data/html/cham-soc-suc-khoe.ts
@@ -0,0 +1,26 @@
+export default `
+<p><em><strong>Trong công cuộc chăm sóc giáo dục toàn diện cho mỗi trẻ nhỏ thì việc bảo vệ giữ gìn chăm sóc sức khỏe vệ sinh cá nhân là yếu tố quan trọng nhất. Để trẻ có thể vệ sinh cá nhân thật tốt thì dụng cụ và sản phẩm chăm sóc sức khỏe là những thứ không thể thiếu. Trẻ em vùng cao hiện nay đang phải đối mặt với việc vệ sinh cá nhân không được đảm bảo vì cơ sở vật chất cũng như dụng cụ chăm sóc đang bị thiếu hụt.</strong></em></p>
+<h3>Hiện trạng cơ sở vật chất các điểm trường tại vùng cao</h3>
+<p>Việc thiếu thốn về cơ sở vật chất gây ảnh hưởng rất lớn đến công tác đổi mới phương pháp dạy học của giáo viên, không đủ không gian để học sinh tiến hành các hoạt động như thảo luận, trao đổi theo nhóm.</p>
+<p>Về mặt sức khỏe, việc sử dụng khu vệ sinh chuẩn và thói quen rửa tay sau khi đi vệ sinh giúp giảm nguy cơ lây nhiễm bệnh đường tiêu hóa, giữ gìn sức khỏe trẻ em, thay vì chỉ là nơi vệ sinh kín đáo. Từ đó, học sinh được gián tiếp nâng cao nhận thức, kiến thức và thực hành phòng bệnh, tạo thói quen giữ vệ sinh chung và vệ sinh cá nhân từ khi còn nhỏ rồi lan tỏa cho gia đình, xã hội.</p>
+<p><img class="aligncenter" src="https://images.baodantoc.vn/uploads/2019/ngay11thang10/baodantoc_trg.jpg" /></p>
+<h3>Hướng dẫn trẻ chăm sóc sức khỏe vệ sinh cá nhân đúng cách</h3>
+<p>Vi khuẩn thường xâm nhập vào cơ thể qua da, gây hại cho sức khỏe vì thế chúng ta cần vệ sinh cá nhân bản thân sạch sẽ giảm thiểu nguy cơ bị vi khuẩn xâm nhập. Chúng ta nên vệ sinh cá nhân bằng cách sử dụng các sản phẩm chăm sóc sức khỏe như: dầu gội, sữa tắm, kem đánh răng… đặc biệt nên chú ý và hình thành cho trẻ thói quen vệ sinh cá nhân hợp lý.</p>
+<ul>
+	<li>
+<p><strong>Rửa tay trước và sau khi ăn uống:</strong> Tay là bộ phận chứa rất nhiều vi khuẩn do tiếp xúc với nhiều nơi rồi lại dùng để chạm lên mặt, cầm nắm thức ăn, vì vậy việc làm sạch tay vô cùng quan trọng để đảm bảo vệ sinh, ngăn ngừa bệnh tiêu chảy và nhiễm trùng đường hô hấp.</p>
+</li>
+	<li>
+<p><strong>Chăm sóc da toàn thân:</strong> Da là cơ quan lớn nhất trong cơ thể, bao phủ và bảo vệ tất cả các cơ quan khác khỏi các yếu tố bên ngoài trong khi tiếp xúc với các loại vi sinh vật khác nhau. Sự tích tụ của vi khuẩn trên da có thể gây ra mùi cơ thể. Vì vậy, cần phải dạy trẻ cách để giữ cho làn da sạch sẽ để tránh các bệnh lý lây nhiễm.</p>
+</li>
+	<li>
+<p><strong>Vệ sinh răng miệng:</strong> Vệ sinh răng miệng cũng quan trọng như vệ sinh tay hoặc vệ sinh da ở trẻ em. Miệng không sạch sẽ có thể tạo ra mùi hôi và gây sâu răng. Ví dụ, việc tạo cho trẻ thói quen đánh răng sáng và tối.</p>
+</li>
+</ul>
+<p><strong><img class="aligncenter" src="https://medlatec.vn/ImagePath/images/20220611/20220611_rua-tay-thuong-xuyen-de-loai-bo-vi-khuan.jpg" /></strong></p>
+<h3>Khoảng Trời Của Bé và chương trình Bé Khỏe Tới Trường tại các điểm trường vùng cao</h3>
+<p>Chúng ta cần giải thích cho trẻ hiểu thế nào là thói quen xấu và tốt để trẻ tránh xa những thói quen xấu, thực hành những thói quen tốt, từ đó hình thành cho trẻ việc phải thường xuyên chú ý đến chăm sóc sức khoẻ và việc vệ sinh cá nhân.</p>
+<p><img class="aligncenter" src="https://hongngochospital.vn/wp-content/uploads/2020/07/ve-sinh-ca-nhan-5.jpg?w=1130" /></p>
+<p>Cha mẹ cũng nên làm gương cho trẻ từ những việc nhỏ nhất như rửa tay, đánh răng,... để khuyến khích trẻ tuân theo những thói quen tốt cho bản thân. Không nên biến việc vệ sinh trở thành một vấn đề khủng khiếp với trẻ nhỏ. Chúng sẽ chỉ hứng thú với việc vệ sinh cá nhân khi việc này đem lại sự vui vẻ, hạnh phúc.</p>
+<p>Đối với trẻ em vùng cao thì việc có được đầy đủ các sản phẩm để vệ sinh cá nhân là một điều không thể. Nhận biết được vấn đề đó, Khoảng Trời Của Bé đã mang đến cho các em nhỏ ở đây những phần quà có chứa những sản phẩm chăm sóc sức khỏe trong chương trình Bé Khỏe Tới Trường giúp các bé chăm sóc và cải thiện sức khỏe tốt hơn cùng với nụ cười trắng sáng</p>
+`
