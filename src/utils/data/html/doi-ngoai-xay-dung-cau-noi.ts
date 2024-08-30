53cd74725acd365eba5631f89ad606139ea7dd45
TaiNguyen297 <tainguyen29702@gmail.com>
2024-08-30 20:21:29 +0700

    doi-ngoai-xay-dung-cau-noi

---
+++ src/utils/data/html/doi-ngoai-xay-dung-cau-noi.ts
@@ -0,0 +1,37 @@
+export default `
+<p><span style="font-size: 12pt;"><em><strong>Ban Đối ngoại của Dự án Khoảng Trời Của Bé là một vai trò quan trọng trong việc xây dựng và duy trì mối quan hệ với các đối tác, nhà tài trợ, và các tổ chức liên quan. Thành viên ban Đối ngoại sẽ là cầu nối giữa dự án và các bên liên quan, đảm bảo sự hỗ trợ và hợp tác hiệu quả cho các hoạt động của dự án. Bởi vậy, nếu bạn là người yêu thích sự kết nối và đam mê tình nguyện thì hãy về chung nhà với chúng mình nhé!</strong></em></span></p>
+<h4><strong><span style="font-size: 12pt;">ĐỐI TƯỢNG THAM GIA</span></strong></h4>
+<ul>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Các bạn trẻ trên địa bàn Hà Nội quan tâm tới các hoạt động ngoại khóa và tình nguyện.</span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Ưu tiên những bạn yêu thích sự kết nối, các hoạt động tình nguyện, xã hội và muốn phát triển kỹ năng giao tiếp, xây dựng mối quan hệ.</span></li>
+</ul>
+<h4><strong><span style="font-size: 12pt;">MÔ TẢ CÔNG VIỆC</span></strong></h4>
+<ul>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Xây dựng và duy trì mối quan hệ với các đối tác, tổ chức phi chính phủ, tổ chức tình nguyện,...</span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Tìm kiếm và đề xuất các đối tác tiềm năng, nhà tài trợ cho các hoạt động của dự án.</span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Tham gia đàm phán và hỗ trợ cho dự án.</span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Phối hợp với các ban khác để thực hiện những công việc hướng tới mục tiêu chung.</span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Lập báo cáo định kỳ về tình hình đối ngoại và hợp tác của dự án.</span></li>
+</ul>
+<h4><strong><span style="font-size: 12pt;">QUYỀN LỢI </span></strong></h4>
+<ul>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Nhận được giấy chứng nhận từ KTCB sau khi hoàn thành tốt nhiệm vụ.</span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Cơ hội gặp gỡ, kết nối với các chuyên gia, tổ chức uy tín trong nhiều lĩnh vực.</span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Phát triển từ các kỹ năng mềm trong quá trình làm việc. </span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Được tiếp cận với môi trường tình nguyện nghiêm túc và chuyên nghiệp, nơi bạn vừa được làm những việc ý nghĩa, vừa có cơ hội phát triển bản thân.</span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Có cơ hội làm quen với nhiều người bạn chung tình yêu với những việc tử tế.</span></li>
+</ul>
+<p><img class="aligncenter wp-image-1038 size-full" src="http://ktcb.top/wp-content/uploads/2024/08/IMG_4871-1.jpg" alt="" width="959" height="719" /></p>
+<h4><strong><span style="font-size: 12pt;">YÊU CẦU</span></strong></h4>
+<ul>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Kỹ năng giao tiếp và có khả năng xử lý tình huống linh hoạt.</span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Có kinh nghiệm hoặc đam mê trong lĩnh vực quan hệ công chúng, đối ngoại là một lợi thế.</span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Khả năng làm việc độc lập và làm việc nhóm tốt, có tinh thần trách nhiệm cao.</span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Biết sử dụng các công cụ văn phòng cơ bản.</span></li>
+	<li style="font-weight: 400;" aria-level="1"><span style="font-weight: 400; font-size: 12pt;">Có khả năng tìm kiếm, lọc thông tin.</span></li>
+</ul>
+<p><span style="font-size: 12pt;">Đội Đối Ngoại mang theo mình sứ mệnh quảng bá những giá trị cốt lõi, tốt đẹp mà Khoảng Trời Của Bé đã xây dựng. KTCB tin rằng, thông qua Đội Đối Ngoại, chúng ta sẽ xây dựng và nhân rộng ngày càng nhiều hơn những chương trình thiết thực cho trẻ em có hoàn cảnh khó khăn. </span></p>
+<p><span style="font-size: 12pt;">Vậy nên, đừng chần chừ, hãy đến và gia nhập vào Đội "Đối Ngoại" của Khoảng Trời Của Bé. Hãy cùng nhau sáng tạo, gây dựng và lan tỏa những thông điệp tích cực trong mọi bước đường ta đi.    </span></p>
+<h4><span style="font-size: 12pt;"><strong>ĐĂNG KÝ NGAY <a href="https://docs.google.com/forms/d/e/1FAIpQLSdnW4kvTgjuLViGyr9rhDe6g8cCPtPoJiw4_ob_fuZjNhzo3g/viewform">TẠI ĐÂY</a>!</strong></span></h4>
+<p>&nbsp;</p>
+`
